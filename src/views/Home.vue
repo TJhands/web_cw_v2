9f0840796595eaeda57e7ed10262ea20a2399e6d
TJhands <taiquan_yuan@foxmail.com>
2020-06-07 16:15:20 +0800

    elementui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" />
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -12,3 +16,39 @@ export default {
   components: {}
 };
 </script>
+
+<style>
+.el-header, .el-footer {
+background-color: #B3C0D1;
+color: #333;
+text-align: center;
+line-height: 60px;
+}
+
+.el-aside {
+background-color: #D3DCE6;
+color: #333;
+text-align: center;
+line-height: 200px;
+}
+
+.el-main {
+background-color: #E9EEF3;
+color: #333;
+text-align: center;
+line-height: 160px;
+}
+
+body > .el-container {
+margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+line-height: 320px;
+}
+</style>
